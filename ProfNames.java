3883656ec906a7c8d66156cf1cf4fb6c4eef1fb5
Kennethyeon1 <600020446@fjuhsd.org>
2021-04-27 16:31:55 +0000

    completed question 2

--- ProfNames.java
+++ ProfNames.java
@@ -2,8 +2,17 @@ public class ProfNames
 {
   public int countNames(String[][]nameArray, String lastName)
   {
-
-
-    return 0;
+    int counter = 0;
+    for (int r = 0; r < nameArray.length; r++)
+    {
+      for (int c = 0; c < nameArray[0].length; c++)
+      {
+          if (nameArray[r][c].substring(0, lastName.length()).equals(lastName))
+          {
+            counter++;
+          }
+      }
+    }
+    return counter;
   }
 }
